ad5a73c421e5bae280b590dc7bfd6c45e5fa177e
ZYN3032 <155139749+ZYN3032@users.noreply.github.com>
2025-02-11 18:21:42 +0800

    Update JournalALL.vue

--- src/components/Publications/Journal/JournalALL.vue
+++ src/components/Publications/Journal/JournalALL.vue
@@ -3,6 +3,12 @@
         <div class="publications_content">
             <ul>
                 <div class="Journal_statement">
+                    <div class="Journal_content_statement">2025</div>
+                </div>
+                <li class="publications_sentence text_area"><b>·</b>&nbsp;&nbsp;[<span style="color: #4472C4;">ml/dm</span>]&nbsp;C. Huo, D. He, Y. Li, D. Jin, J. Dang, W. Pedrycz, L. Wu, and <b>W. Zhang</b>, Heterogeneous Graph Neural Networks using Self-supervised Reciprocally Contrastive Learning, ACM Trans. Intell. Syst. Technol, 2025, 16(1):1-21.</li>
+                <li class="publications_sentence text_area"><b>·</b>&nbsp;&nbsp;[<span style="color: #BF8F00;">medImg</span>]&nbsp;H. Guo, Y. Xiao, S. Dong, J. Yang, P. Zhao, T. Zhao, A. Cai, L. Tang, J. Liu, H. Wang, R. Hua, R. Liu, Y. Wei, D. Sun, Z. Liu, M. Xia, Y. He, Y. Wu, T. Si, F. Womer, F. Xu, Y. Tang, J. Wang, <b>W. Zhang</b>, X. Zhang & F. Wang , Bridging animal models and humans: neuroimaging as intermediate phenotypes linking genetic or stress factors to anhedonia. BMC medicine, 2025, 23(1):38.</li>
+                                
+                <div class="Journal_statement">
                     <div class="Journal_content_statement">2024</div>
                 </div>
                 <li class="publications_sentence text_area"><b>·</b>&nbsp;&nbsp;[<span style="color: #4472C4;">ml/dm</span>]&nbsp;D. He, C. Liang, C. Huo, Z. Feng, D. Jin, L. Yang, and <b>W. Zhang</b>, Analyzing heterogeneous networks with missing attributes by unsupervised contrastive learning, IEEE Trans. Neural Networks and Learning Systems, 2024, 35(4):4438-50.</li>
