4f606afc139d1ccce9a51db29687a003f0fd814c
ZYN3032 <155139749+ZYN3032@users.noreply.github.com>
2025-02-11 18:22:09 +0800

    Add files via upload

---
+++ src/components/Publications/Journal/Journal2025.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="publications_content_year">
+        <div class="publications_content">
+            <ul>
+                <div class="Journal_statement">
+                    <div class="Journal_content_statement">2025</div>
+                </div>
+                <li class="publications_sentence text_area"><b>·</b>&nbsp;&nbsp;[<span style="color: #4472C4;">ml/dm</span>]&nbsp;C. Huo, D. He, Y. Li, D. Jin, J. Dang, W. Pedrycz, L. Wu, and <b>W. Zhang</b>, Heterogeneous Graph Neural Networks using Self-supervised Reciprocally Contrastive Learning, ACM Trans. Intell. Syst. Technol, 2025, 16(1):1-21.</li>
+                <li class="publications_sentence text_area"><b>·</b>&nbsp;&nbsp;[<span style="color: #BF8F00;">medImg</span>]&nbsp;H. Guo, Y. Xiao, S. Dong, J. Yang, P. Zhao, T. Zhao, A. Cai, L. Tang, J. Liu, H. Wang, R. Hua, R. Liu, Y. Wei, D. Sun, Z. Liu, M. Xia, Y. He, Y. Wu, T. Si, F. Womer, F. Xu, Y. Tang, J. Wang, <b>W. Zhang</b>, X. Zhang & F. Wang , Bridging animal models and humans: neuroimaging as intermediate phenotypes linking genetic or stress factors to anhedonia. BMC medicine, 2025, 23(1):38.</li>
+             
+            </ul>
+        </div>
+    </div>
+
+ </template>
+
+ <script>
+    export default {
+    }
+</script>
+
+<style lang="less" scoped>
+
+
+.text_area{
+    box-shadow: 0 3px 8px rgba(0, 0, 0, 0.05);
+    .hoverShadow();
+}
+</style>
